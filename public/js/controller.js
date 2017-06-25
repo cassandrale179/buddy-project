126badada1568766bbd575aac48f122009a0c8dc
lenguyentri1999 <michaelle7312@gmail.com>
2017-06-25 14:54:33 -0400

    Finished forgot password

--- public/js/controller.js
+++ public/js/controller.js
@@ -7,27 +7,20 @@ angular.module('app.controllers', [])
   }])
 
 //--------------------  CONTROLLER FOR THE FOROT PASSWORD PAGE --------------------
-.controller('forgotPageCtrl', ['scope', '$state',
+.controller('forgotPageCtrl', ['$scope', '$state',
   function($scope, $state){
-    var user = firebase.auth().currentUser;
-    var name, email, photoUrl, uid, emailVerified;
+    $scope.ResetPassword = function()
+    {
+      var auth = firebase.auth();
+      auth.sendPasswordResetEmail($scope.txtEmail).then(function() {
+        console.log("Password reset email sent!");
 
-    if (user != null) {
-      name = user.displayName;
-      email = user.email;
-      photoUrl = user.photoURL;
-      emailVerified = user.emailVerified;
-      uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-                       // this value to authenticate with your backend server, if
-                       // you have one. Use User.getToken() instead.
+      }, function(error) {
+        console.log("An error happened!";
+      })
+
     }
-    var auth = firebase.auth();
-    auth.sendPasswordResetEmail(email).then(function() {
-      console.log("Password reset email sent!");
 
-    }, function(error) {
-      console.log('An error happened.');
-    })
 
   }])
 
