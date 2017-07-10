fc7b7e9a89988341ddb3030b5cbbbcace1351458
Minh Le <lenguyetminh1998@gmail.com>
2017-07-10 08:31:08 -0400

    Add in search page

--- public/js/controller/search.js
+++ public/js/controller/search.js
@@ -1,10 +1,30 @@
-app.controller('searchPageCtrl', ['$scope', '$state', '$localStorage',
-  function ($scope, $state, $localStorage){
-    var user = firebase.auth().currentUser;
-    if (user===null){
+app.controller('searchPageCtrl', ['$scope', '$state', '$localStorage', '$firebaseArray',
+  function ($scope, $state, $localStorage, $firebaseArray)
+  {
+    $scope.nameData = [];
+    $scope.emailData = [];
+
+    //IF USER HASN'T LOGGED IN YET, THEN LOG THEM IN
+    var currentUser = firebase.auth().currentUser;
+    if (currentUser===null){
       firebase.auth().signInWithEmailAndPassword($localStorage.email, $localStorage.password).then(function(){
         $state.reload();
       });
+    }
+
+    else{
+      var refUser = firebase.database().ref("users");
+      refUser.once('value', function(snapshot){
+        var table = snapshot.val();
+        for (var user in table){
+          $scope.nameData.push(table[user].name);
+          $scope.emailData.push(table[user].email);
+        }
+        console.log($scope.nameData);
+        console.log($scope.emailData);
+      });
+
+
     }
   }
 ]);
