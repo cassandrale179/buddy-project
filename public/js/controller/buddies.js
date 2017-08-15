432acd8e40b53c72bcfedf40587cd000c406f72e
Minh Le <lenguyetminh1998@gmail.com>
2017-08-15 18:28:05 -0400

    Finish redesign of buddies page

--- public/js/controller/buddies.js
+++ public/js/controller/buddies.js
@@ -57,19 +57,56 @@ app.controller('buddiesPageCtrl', ['$scope', '$state', '$localStorage', '$fireba
       });
 
       //----------- WHEN USER CLICK ADD AN INTEREST -----------------
-      $scope.CaptureInterest = function(){
+      $scope.AutoFill = function(interest){
+        $scope.searchInterest = interest.name;
+      };
 
+      //----------- WHEN USER CLICK ADD AN INTEREST -----------------
+      $scope.CaptureInterest = function(){
         if (!$scope.searchInterest){
           $scope.errorMessage = "Please input an interest";
           return;}
         if ($scope.interestArr.indexOf($scope.searchInterest) == -1){
           $scope.interestArr.push($scope.searchInterest);
           $scope.searchInterest = null;
+          $scope.errorMessage = "";
         }
         else{
           $scope.errorMessage = "You already added this interest";
         }
+      };
 
+      //-------- WHEN USER WANT TO REMOVE AN INTEREST --------------
+      $scope.Remove = function(x){
+        $scope.interestArr.splice(x, 1);
+      };
+
+
+      //-------- WHEN USER CLICK SEARCH FOR BUDDIES --------------
+      $scope.SubmitInterest = function(){
+
+        //ADD THEIR INTEREST AS A STRING IN THE USER TABLE
+        refUserId.once('value', function(snapshot){
+          var info = snapshot.val().age;
+          var interestStr = info.interest;
+          if (!info.Interest){
+            interestStr="";
+          }
+          for (var k = 0; k< $scope.interestArr.length; k++){
+            interestStr+=$scope.interestArr[k] + ',';
+          }
+          refUserId.update({interest: interestStr});
+          console.log(interestStr);
+
+          //ADD THEIR INTEREST TO THE INTERESTS TABLE
+          for (var i = 0; i<$scope.interestArr.length;i++){
+            var currentInterest = $scope.interestArr[i];
+            var ref = firebase.database().ref("interests/"+currentInterest+"/" +currentUser.uid);
+            ref.set(info);
+          }
+        });
+
+        $state.go('profile');
       };
     }
   }
